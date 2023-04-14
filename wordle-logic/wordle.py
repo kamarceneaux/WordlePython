6b53dcb99022669bb3147a8b939af9bff6f4fe3b
Kameron Arceneaux <kameronarceneaux05@Gmail.com>
2023-04-14 18:33:17 -0500

    started to work on wordle class

--- wordle-logic/wordle.py
+++ wordle-logic/wordle.py
@@ -1,4 +1,20 @@
+import random
+
+
 class Wordle:
 
-    def __init__(self):
-        """Contain the logic for the words that are available to be used."""
+    def __init__(self, words: list[str]):
+        self.words = words
+        self.guess_attempts = 5
+        self.current_answer = []
+        self.correct_answer = " "
+        self.correct_answer_arr = None
+
+    def selectWord(self) -> str:
+        """
+        Selects a word from our list contain words and returns
+        :return:
+        """
+        self.correct_answer = random.choice(self.words)
+
+        self.correct_answer_arr = list(self.correct_answer)
